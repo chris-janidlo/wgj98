400167a4027d5c256be796385029ee6b939ab4f7
crass_sandwich <christopherjanidlo@gmail.com>
2019-05-25 23:54:05 -0400

    Idle gameplay

---
+++ Assets/Scripts/Bank.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using crass;
+
+public class Bank : Singleton<Bank>
+{
+    public AnimationCurve MoneyGainByHappiness;
+
+    public float SecondsPerTick = 1;
+
+    [SerializeField]
+    float _money = 1;
+    public int Money => (int) _money;
+
+    float ticker;
+    
+    void Awake ()
+    {
+        if (SingletonGetInstance() != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            SingletonSetInstance(this, false);
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Update ()
+    {
+        ticker += Time.deltaTime;
+        if (ticker >= SecondsPerTick)
+        {
+            ticker = 0;
+            CompoundMoney(MoneyGainByHappiness.Evaluate(DragonStats.Instance.Happiness));
+        }
+
+    }
+
+    public void SetMoney (float value)
+    {
+        Assert.IsTrue(value >= 0, "can't have negative money");
+        _money = value;
+    }
+
+    public void IncrementMoney (float delta)
+    {
+        SetMoney(_money + delta);
+    }
+
+    public void CompoundMoney (float percent)
+    {
+        IncrementMoney(_money * percent);
+    }
+}
---
+++ Assets/Scripts/DragonStats.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Assertions;
+using crass;
+
+public class DragonStats : Singleton<DragonStats>
+{
+    [Serializable]
+    public class Meter
+    {
+        [SerializeField]
+        float _value = 100;
+        public float Value
+        {
+            get => _value;
+            set
+            {
+                _value = Mathf.Clamp(value, 0, 100);
+            }
+        }
+
+        public float DecayRate, HappinessWeight;
+        
+        public float WeightedValue => Value * HappinessWeight;
+
+        public void Update ()
+        {
+            Value -= DecayRate * Time.deltaTime;
+        }
+    }
+
+    public Meter Cleanliness, Hunger, Love;
+
+    public float Happiness
+    {
+        get
+        {
+            Assert.AreEqual(Cleanliness.HappinessWeight + Hunger.HappinessWeight + Love.HappinessWeight, 1);
+
+            return Cleanliness.WeightedValue + Hunger.WeightedValue + Love.WeightedValue;
+        }
+    }
+
+    void Awake ()
+    {
+        if (SingletonGetInstance() != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            SingletonSetInstance(this, false);
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Update ()
+    {
+        Cleanliness.Update();
+        Hunger.Update();
+        Love.Update();
+    }
+}
