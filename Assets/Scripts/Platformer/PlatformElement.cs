9054e00db726a1d3213bac49be77ec8b6822616c
crass_sandwich <christopherjanidlo@gmail.com>
2019-05-29 01:04:50 -0400

    Platformer stuff

---
+++ Assets/Scripts/Platformer/PlatformElement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformElement : MonoBehaviour
+{
+    const float spawnTime = .5f;
+
+    IEnumerator Start ()
+    {
+        float origScale = ((Vector2) transform.localScale).magnitude;
+        transform.localScale = Vector2.zero;
+
+        float timer = spawnTime;
+        while (timer >= 0)
+        {
+            float size = Mathf.Lerp(0, origScale, 1 - (timer / spawnTime));
+            transform.localScale = new Vector3(size, size, 1);
+            timer -= Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = new Vector3(origScale, origScale, 1);
+    }
+}
---
+++ Assets/Scripts/Platformer/PlatformerGem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerGem : MonoBehaviour
+{
+    public float Value = 1;
+    
+    void OnTriggerEnter2D ()
+    {
+        Bank.Instance.IncrementMoney(Value);
+        Destroy(gameObject);
+    }
+}
