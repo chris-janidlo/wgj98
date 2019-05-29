e648b0da830037548ed1bb19bef74e382df295d6
crass_sandwich <christopherjanidlo@gmail.com>
2019-05-28 22:25:09 -0400

    Idle animations

---
+++ Assets/Scripts/Idle/DragonEmoting.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonEmoting : MonoBehaviour
+{
+    public Animator Animator;
+
+    void Update ()
+    {
+        Animator.SetFloat("Happiness", DragonStats.Instance.Happiness);
+    }
+}
---
+++ Assets/Scripts/Idle/Floater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    public float VertPeriod, VertAmplitude, RotPeriod, RotAmplitude;
+
+    float startY;
+
+    void Start ()
+    {
+        startY = transform.position.y;    
+    }
+
+    void Update ()
+    {
+        var newY = VertAmplitude * Mathf.Sin((2 * Mathf.PI / VertPeriod) * Time.time) + startY;
+        transform.position = new Vector2(transform.position.x, newY);
+
+        var newRot = RotAmplitude * Mathf.Sin((2 * Mathf.PI / RotPeriod) * Time.time);
+        transform.rotation = Quaternion.Euler(Vector3.forward * newRot);
+    }
+}
