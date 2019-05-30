042003c0c8030c580c91db306c4413e800a03ea6
crass_sandwich <christopherjanidlo@gmail.com>
2019-05-29 22:53:16 -0400

    Add shop

---
+++ Assets/Scripts/Idle/Shop.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [Serializable]
+    public class Inflation
+    {
+        [Serializable]
+        public class InflationPoint
+        {
+            public int ActivatingWorth, Price;
+        }
+
+        public List<InflationPoint> InflationData;
+        public int Counter;
+
+        public int GetCurrentPrice ()
+        {
+            if (Counter <= InflationData.Count && Bank.Instance.Money >= InflationData[Counter].ActivatingWorth)
+            {
+                Counter++;
+            }
+
+            return InflationData[Counter].Price;
+        }
+    }
+
+    public Inflation StrawberryInflation, StrawberryPairInflation, CleanerInflation;
+
+    public int BookOnePrice, BookTwoPrice, BookThreePrice;
+
+    public ShopButton Strawberry, StrawberryPair, Cleaner, BookOne, BookTwo, BookThree;
+
+    bool openState;
+
+    void Start ()
+    {
+        Strawberry.Button.onClick.AddListener(() => Inventory.Instance.Strawberries++);
+        StrawberryPair.Button.onClick.AddListener(() => Inventory.Instance.StrawberryPairs++);
+        Cleaner.Button.onClick.AddListener(() => Inventory.Instance.CleaningSupplies++);
+
+        BookOne.Button.onClick.AddListener(() => { Inventory.Instance.BookOne = true; removeOwnedBooks(); });
+        BookTwo.Button.onClick.AddListener(() => { Inventory.Instance.BookTwo = true; removeOwnedBooks(); });
+        BookThree.Button.onClick.AddListener(() => { Inventory.Instance.BookThree = true; removeOwnedBooks(); });
+
+        BookOne.Price = BookOnePrice;
+        BookTwo.Price = BookTwoPrice;
+        BookThree.Price = BookThreePrice;
+        applyInflation();
+    }
+
+    void Update ()
+    {
+        if (Input.GetMouseButton(0) && !RectTransformUtility.RectangleContainsScreenPoint(GetComponent<RectTransform>(), Input.mousePosition))
+        {
+            SetOpenState(false);
+        }
+    }
+    
+    public void SetOpenState (bool value)
+    {
+        if (openState == value) return;
+
+        openState = value;
+
+        if (value)
+        {
+            applyInflation();
+            removeOwnedBooks();
+        }
+
+        transform.GetChild(0).gameObject.SetActive(value);
+    }
+
+    void applyInflation ()
+    {
+        Strawberry.Price = StrawberryInflation.GetCurrentPrice();
+        StrawberryPair.Price = StrawberryPairInflation.GetCurrentPrice();
+        Cleaner.Price = CleanerInflation.GetCurrentPrice();
+    }
+
+    void removeOwnedBooks ()
+    {
+        if (Inventory.Instance.BookOne)
+        {
+            BookOne.gameObject.SetActive(false);
+        }
+
+        if (Inventory.Instance.BookTwo)
+        {
+            BookTwo.gameObject.SetActive(false);
+        }
+
+        if (Inventory.Instance.BookThree)
+        {
+            BookThree.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Idle/ShopButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShopButton : MonoBehaviour
+{
+    public Button Button;
+    public TextMeshProUGUI Text;
+    
+    int _price;
+    public int Price
+    {
+        get => _price;
+        set
+        {
+            _price = value;
+            Text.text = $"x {_price}";
+        }
+    }
+
+    void Start ()
+    {
+        Button.onClick.AddListener(() => Bank.Instance.IncrementMoney(-Price));
+    }
+
+    void Update ()
+    {
+        Button.interactable = Bank.Instance.Money >= Price;
+    }
+}
