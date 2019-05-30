fd6b6ca1e835ce04112f02118a03a3ec63db6e17
crass_sandwich <christopherjanidlo@gmail.com>
2019-05-30 11:55:14 -0400

    Main menu

---
+++ Assets/Scripts/MenuLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLoader : MonoBehaviour
+{
+    public float Delay;
+
+    IEnumerator Start ()
+    {
+        yield return new WaitForSeconds(Delay);
+        while (true)
+        {
+            if (Input.anyKey)
+            {
+                SceneManager.LoadScene("IntroCutscene");
+            }
+            yield return null;
+        }
+    }
+}
