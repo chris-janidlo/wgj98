730468cd640dfbffcf35def53c82f764e8e5bca5
crassSandwich <christopherjanidlo@gmail.com>
2019-05-31 17:31:14 -0400

    Controls diagram

---
+++ Assets/Scripts/Platformer/ControlDiagramHighlighter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ControlDiagramHighlighter : MonoBehaviour
+{
+    public Color OnColor, OffColor;
+    public Image Up, Left, Right;
+    public TextMeshProUGUI UpText, LeftText, RightText;
+
+    void Update ()
+    {
+        Up.color = Input.GetButton("Flap") ? OnColor : OffColor;
+        UpText.color = Up.color;
+
+        bool turning = Input.GetButton("Horizontal");
+        float axis = Input.GetAxisRaw("Horizontal");
+
+        Left.color = turning && axis != 1 ? OnColor : OffColor;
+        LeftText.color = Left.color;
+
+        Right.color = turning && axis != -1 ? OnColor : OffColor;
+        RightText.color = Right.color;
+    }
+}
